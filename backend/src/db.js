9ae75d00d2c5acc1745b457cb849c97dbaed9ae4
MohammadMitwaly <mhmadmtwale@gmail.com>
2020-06-09 13:42:33 +0300

    first commit

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// This file connects us to the Prisma DB. Giving us the abillity tto query it using Node.js
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false,
+});
+
+module.exports = db;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+
+import Link from "next/link"
+
+const Sell = props =>(
+    <div>
+        <h1>Sell!</h1>
+    </div>
+);
+
+export default Sell
